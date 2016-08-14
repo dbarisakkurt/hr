bd411f5e639f418c94bdb2570e348d6c32136b82
Baris Akkurt <dbarisakkurt@gmail.com>
2016-08-14 10:17:12 +0300

    Compare the triplets solution

---
+++ Hackerrank/Hackerrank/Algorithms_Warmup_CompareTriplets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank
+{
+    class Algorithms_Warmup_CompareTriplets
+    {
+
+        internal static void CompareAndPrintResult(int[] alicePoints, int[] bobPoints)
+        {
+            int alice = 0, bob = 0;
+            if (alicePoints.Length != bobPoints.Length)
+                throw new Exception("Array must have equal lengths");
+
+            for(int i=0; i<alicePoints.Length; i++)
+            {
+                if (alicePoints[i] > bobPoints[i])
+                    alice += 1;
+                else if (bobPoints[i] > alicePoints[i])
+                    bob += 1;
+            }
+
+            Console.WriteLine(alice + " " + bob);
+        }
+    }
+}
--- Hackerrank/Hackerrank/Program.cs
+++ Hackerrank/Hackerrank/Program.cs
@@ -10,12 +10,16 @@ namespace Hackerrank
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32("6");
-            string[] arr_temp = "1 2 3 4 10 11".Split(' ');
-            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            string[] tokens_a0 = "5 6 7".Split(' ');
+            int a0 = Convert.ToInt32(tokens_a0[0]);
+            int a1 = Convert.ToInt32(tokens_a0[1]);
+            int a2 = Convert.ToInt32(tokens_a0[2]);
+            string[] tokens_b0 = "3 6 10".Split(' ');
+            int b0 = Convert.ToInt32(tokens_b0[0]);
+            int b1 = Convert.ToInt32(tokens_b0[1]);
+            int b2 = Convert.ToInt32(tokens_b0[2]);
 
-            int result = Algorithms_Warmup_SimpleArraySum.Sum(arr, n);
-            Console.WriteLine(result);
+            Algorithms_Warmup_CompareTriplets.CompareAndPrintResult(new int[] { a0, a1, a2 }, new int[] { b0, b1, b2});
             Console.ReadLine();
 
         }
