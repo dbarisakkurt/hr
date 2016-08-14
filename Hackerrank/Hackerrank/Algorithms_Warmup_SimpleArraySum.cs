f8367b2d50629b3706be6970e0dcd69c32a300b4
Baris Akkurt <dbarisakkurt@gmail.com>
2016-08-14 08:56:41 +0300

    algorithms-warmup-simple array sum solution

---
+++ Hackerrank/Hackerrank/Algorithms_Warmup_SimpleArraySum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank
+{
+    class Algorithms_Warmup_SimpleArraySum
+    {
+        internal static int Sum(int[] array, int length)
+        {
+            if (length > array.Length)
+                throw new Exception("Given number is larger than array length");
+
+            int total = 0;
+            for(int i=0; i<length; i++)
+            {
+                total += array[i];
+            }
+            return total;
+
+        }
+    }
+}
--- Hackerrank/Hackerrank/Program.cs
+++ Hackerrank/Hackerrank/Program.cs
@@ -10,6 +10,14 @@ namespace Hackerrank
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32("6");
+            string[] arr_temp = "1 2 3 4 10 11".Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            int result = Algorithms_Warmup_SimpleArraySum.Sum(arr, n);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
         }
     }
 }
