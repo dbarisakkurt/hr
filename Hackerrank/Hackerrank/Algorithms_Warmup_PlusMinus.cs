79b1ea4e23780d0c84e6e5618c3e604f1dad2e97
Baris Akkurt <dbarisakkurt@gmail.com>
2016-09-18 11:57:39 +0300

    Algorithm Warmup Plus Minus and Staircase solutions

---
+++ Hackerrank/Hackerrank/Algorithms_Warmup_PlusMinus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hackerrank
+{
+    //Hackerrank
+    //Algorithms / Warmup / Plus Minus
+    static class Algorithms_Warmup_PlusMinus
+    {
+        public static void PlusMinus(int n)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            int zero = 0, pos = 0, neg = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    pos += 1;
+                }
+                else if (arr[i] == 0)
+                {
+                    zero += 1;
+                }
+                else
+                {
+                    neg += 1;
+                }
+            }
+            Console.WriteLine((double)pos / n);
+            Console.WriteLine((double)neg / n);
+            Console.WriteLine((double)zero / n);
+        }
+    }
+}
---
+++ Hackerrank/Hackerrank/Algorithms_Warmup_Staircase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Hackerrank
+{
+    static class Algorithms_Warmup_Staircase
+    {
+        //Hackerrank
+        //Algorithms / Warmup / Staircase
+        public static void Staircase(int n)
+        {
+            int k = n - 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j >= k)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                k -= 1;
+                Console.WriteLine();
+            }
+        }
+    }
+}
