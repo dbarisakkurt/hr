47d1c4840f9a7b1aa607043a47f4398e343647c8
Baris Akkurt <dbarisakkurt@gmail.com>
2016-09-22 20:58:50 +0300

    Algorithms Warmup Kangoroo and Divisible Sum Pairs solutions

---
+++ Hackerrank/Hackerrank/Algorithms_Warmup_DivisibleSumPairs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank
+{
+    //Hackerrank
+    //Algorithms / Warmup / Divisible Sum Pairs Question
+    static class Algorithms_Warmup_DivisibleSumPairs
+    {
+        public static void DivisibleSumPairs()
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            int counter = 0;
+            int m = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = m; j < n; j++)
+                {
+                    if ((a[i] + a[j]) % k == 0)
+                        counter += 1;
+                }
+                m += 1;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Hackerrank/Hackerrank/Algorithms_Warmup_Kangoroo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Hackerrank
+{
+    static class Algorithms_Warmup_Kangoroo
+    {
+        //Hackerrank
+        //Algorithms / Warmup / Kangoroo Question
+        public static void Kangoroo()
+        {
+            string[] tokens_x1 = Console.ReadLine().Split(' ');
+            int x1 = Convert.ToInt32(tokens_x1[0]);
+            int v1 = Convert.ToInt32(tokens_x1[1]);
+            int x2 = Convert.ToInt32(tokens_x1[2]);
+            int v2 = Convert.ToInt32(tokens_x1[3]);
+            bool samePlace = false;
+
+            if (x2 > x1 && v2 > v1)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            else
+            {
+                if (x1 < x2)
+                {
+                    while (x1 <= x2 || samePlace == true)
+                    {
+                        x1 += v1;
+                        x2 += v2;
+                        if (x1 == x2)
+                        {
+                            samePlace = true;
+                            break;
+                        }
+                        if (x1 > x2)
+                            break;
+                    }
+                }
+                else
+                { //x1>=x2
+                    while (x1 >= x2 || samePlace == true)
+                    {
+                        x1 += v1;
+                        x2 += v2;
+                        if (x1 == x2)
+                        {
+                            samePlace = true;
+                            break;
+                        }
+                        if (x1 < x2)
+                            break;
+                    }
+                }
+            }
+
+            if (samePlace)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
